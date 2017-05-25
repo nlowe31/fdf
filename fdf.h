141fc26be924ea9a303cf4ab5b7918c9338e95ed
nlowe31 <nlowe@e2r11p12.42.fr>
2017-05-25 19:46:45 +0200

    initial commit

--- fdf.h
+++ fdf.h
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/11 20:53:14 by nlowe             #+#    #+#             */
-/*   Updated: 2017/05/25 18:53:04 by nlowe            ###   ########.fr       */
+/*   Created: 2017/05/25 18:53:32 by nlowe             #+#    #+#             */
+/*   Updated: 2017/05/25 18:58:57 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fdf.h"
+#ifndef FDF_H
 
-int		fdf_error(int exit, int arnaud)
-{
-	if (arnaud)
-		ft_dprintf(2, "fdf: %s\n", strerror(arnaud));
-	else
-		perror("fdf");
-	if (exit)
-		exit(0);
-	return (1);
-}
+# include "libft.h"
 
-int		main(void)
-{
-	void	*mlx;
-	void	*win;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 400, 400, "MLX Test");
-	mlx_loop(mlx);
-}
+
+int		fdf_error(int exit, int arnaud);
+
+#endif
